6417a3548674ea72dc604e65ce88246b97213a37
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-09 18:57:52 +0200

    imps object

---
+++ src/components/pics/Imgs.js
@@ -0,0 +1,26 @@
+import logo from "./logo.png";
+import bag1 from "./bag1.png";
+import bag2 from "./bag2.png";
+import pants from "./pants.png";
+import sock1 from "./SOCK1.png";
+import sock3 from "./SOCK3.png";
+import sock5 from "./SOCK5.png";
+import tshirt1 from "./Tshirt1.jpg";
+import tshirt2 from "./Tshirt2.jpg";
+import tshirt3 from "./Tshirt3.jpg";
+import tshirt8 from "./Tshirt8.jpg";
+import svg from "./community.svg";
+export const imgs = {
+  logo,
+  bag1,
+  bag2,
+  pants,
+  sock1,
+  sock3,
+  sock5,
+  tshirt1,
+  tshirt2,
+  tshirt3,
+  tshirt8,
+  svg,
+};
