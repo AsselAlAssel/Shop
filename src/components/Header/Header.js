670168a49f739a1c9aec7754cd2670dd09014fc9
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-09 17:37:38 +0200

    header and nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,23 @@
 import React from "react";
 import classes from "./Header.module.scss";
+import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <React.Fragment>
-  
-        <marquee behavior="" >
-          <span> CHECK OUT OUR OFFICIAL PEANUTS COLLABORATION!</span>
-        </marquee>
-        <ul className={classes.header}>
-            <li></li>
-            <li><img src={require('./pics/logo.png')} alt="" /></li>
-
-        </ul>
-     
+      <div className={classes.header}>
+        <div className={classes.menu}></div>
+        <div className={classes.logo}>
+          <NavLink to="/">
+            <img src={require("../pics/logo.png")} alt="logo" />
+          </NavLink>
+        </div>
+        <div className={classes.cart}>
+          <img src={require("../pics/cart.png")} alt="" />
+        </div>
+      </div>
+      <marquee>
+        <span> CHECK OUT OUR OFFICIAL PEANUTS COLLABORATION!</span>
+      </marquee>
     </React.Fragment>
   );
 };
