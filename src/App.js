85a528df31f1a00bc1b6662a9db6819db1a5c85b
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-09 18:58:09 +0200

    add main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header/>
+      <Main/>
     </BrowserRouter>
   );
 };
